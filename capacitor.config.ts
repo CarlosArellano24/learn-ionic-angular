eb3bd5e4295d0fbd55164d7f56fe06ce045fa929
Carlos Arellano <carlosfarellano@hotmail.com>
2021-11-28 13:43:45 -0600

    Before downloading dependencies

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,7 +1,7 @@
 import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
+  appId: 'com.carlosafarellano.LearnIonicAngular',
   appName: 'ionic-social',
   webDir: 'www',
   bundledWebRuntime: false
